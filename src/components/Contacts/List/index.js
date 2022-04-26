695fb59c89596f143a35c61fdbf8796caa501d9f
saidugurlu <ugurlumsaid@gmx.de>
2022-04-26 23:02:57 +0200

    filter added

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import {useState} from "react";
 
 function List({ contacts }) {
+  const [filterText, setFilterText]= useState("")
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key)=> 
+      item[key].toString().toLowerCase().includes(filterText.toLowerCase())
+    )
+  })
   return (
     <div>
+
+      <input placeholder="Filter" value ={filterText} onChange={(e)=> setFilterText(e.target.value)}/> 
       <ul>
-        {contacts.map((contact, index) => (
+        {filtered.map((contact, index) => (
           <li key={index}>{contact.fullname}</li>
         ))}
       </ul>
