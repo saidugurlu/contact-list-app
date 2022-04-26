2a86ddb096afe310bb7b583d71150bc2716881a3
saidugurlu <ugurlumsaid@gmx.de>
2022-04-26 19:59:51 +0200

    form added

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useState } from "react";
 
 function Form() {
+  const [form, setForm] = useState({ fullname: "", phoneNumber: "" });
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (form.fullname === "" || form.phoneNumber === "") return false;
+  };
   return (
-    <div>Contact Form</div>
-  )
-};
-export default Form
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          name="fullname"
+          placeholder="Fullname"
+          onChange={onChangeInput}
+        ></input>
+      </div>
+      <div>
+        <input
+          name="phoneNumber"
+          placeholder="Phone Number"
+          onChange={onChangeInput}
+        ></input>
+      </div>
+      <div>
+        <button> Add </button>
+      </div>
+    </form>
+  );
+}
+export default Form;
